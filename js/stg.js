c6858ec6ce4e44914b343ecd109bdbead8e898d1
Nicke Manarin <nicke@outlook.com.br>
2017-01-25 10:49:43 -0200

    Update stg.js

--- js/stg.js
+++ js/stg.js
@@ -53,6 +53,7 @@ function InitTooltip(obj, fadeDelay = 300) {
     });
 
     if (obj.is(":hover")) {
+
         obj.tooltip("show");
     }
 }
@@ -102,3 +103,30 @@ $(document).ready(function () {
 
     GetLatestReleaseInfo();
 });
+
+ var getFirstBrowserLanguage = function () {
+    var nav = window.navigator,
+    browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+    i,
+    language;
+
+    //Support for HTML 5.1 "navigator.languages".
+    if (Array.isArray(nav.languages)) {
+      for (i = 0; i < nav.languages.length; i++) {
+        language = nav.languages[i];
+        if (language && language.length) {
+          return language;
+        }
+      }
+    }
+
+    //Support for other well known properties in browsers.
+    for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+      language = nav[browserLanguagePropertyKeys[i]];
+      if (language && language.length) {
+        return language;
+      }
+    }
+
+    return null;
+  };
